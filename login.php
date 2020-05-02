b97d48798d1ee8ede2356f37b9aa363b779a91cb
Veloz-X <viralveloz@gmail.com>
2020-05-01 20:30:52 -0500

    Crear Login

--- login.php
+++ login.php
@@ -7,10 +7,16 @@
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <div class="contenedor-from">
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST"></form>
-    
-    </div>
+<div class="contenedor-from">
+    <h1>Login</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+    <input type="text" name="Usuario" class="input-control" placeholder="Usuario">
+    <input type="text" name="pass" class="input-control" placeholder="Contraseña">
+    <input type="submit" name="acceder" class="log-btn" value="Iniciar Sesion">
+    <a href="registrar.php"><p>No tengo Cuenta</p></a>
+    </form>
+</div>
+
 
     
 </body>
